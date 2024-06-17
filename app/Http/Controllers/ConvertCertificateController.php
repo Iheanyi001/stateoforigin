049851eed7c40ab5de7d3cb355048b9ec9ae4f02
Iheanyi <ukoiheanyiking@gmail.com>
2024-06-17 09:50:09 +0100

    convertcert

--- app/Http/Controllers/ConvertCertificateController.php
+++ app/Http/Controllers/ConvertCertificateController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ConvertCertificateController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('convert-certificate');
+    }
 }
