91848057ba459fc2df71e23289684250dfc53073
Iheanyi <ukoiheanyiking@gmail.com>
2024-06-17 09:51:54 +0100

    convertcert01

--- resources/views/components/authentication-card.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="d-flex flex-column align-items-center justify-content-center pt-5">
-    <div class="header">
-        Certificate of Origin System
-    </div>
-    <div class="login-wrapper wd-300 wd-xs-450 pd-25 pd-xs-40 bg-white rounded shadow-base mb-5">
-        <div class="signin-logo tx-center tx-28 tx-bold tx-inverse mb-3"></span>LOGIN</div>
-    {{ $slot }}
-    </div>
-</div>
